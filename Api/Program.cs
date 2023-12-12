a2f6458c2e6336f732190accba4a6de63ce95d7c
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2023-12-12 07:16:00 +0100

    jsonoptions

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,6 @@
 using MetadataAttribute.Models;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+   // options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;       //comment to test JsonIgnore attribute
+    options.SerializerOptions.WriteIndented = true;
+});
 
 var app = builder.Build();
 
@@ -18,27 +26,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.MapGet("/books", () =>
+app.MapGet("/book", () =>
 {
     var author1 = new Author { Id = 1, FirstName = "John", LastName = "Doe" };
     var author2 = new Author { Id = 2, FirstName = "Jane", LastName = "Smith" };
@@ -55,12 +44,8 @@ app.MapGet("/books", () =>
 
     return Results.Ok(book);
 })
-
+.WithName("GetBook")
 .WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- Models/Partial/Author.cs
+++ Models/Partial/Author.cs
@@ -6,8 +6,8 @@ namespace MetadataAttribute.Models;
 
 // Tried both Attributes...seems asp.net core use ModelMetadataType instead MetadataType
 
-//[ModelMetadataType(typeof(AuthorMetadata))]
-[MetadataType(typeof(AuthorMetadata))]
+[ModelMetadataType(typeof(AuthorMetadata))]
+//[MetadataType(typeof(AuthorMetadata))]
 public partial class Author
 {
   
